1c1282cd76ebea5627f126ea7bbf748e4baf3f1b
Karolis Kalantojus <karolis@versada.eu>
2019-03-14 11:02:22 +0200

    test module

---
+++ test_module/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2019 Versada UAB
+# Odoo Proprietary License v1.0 or later 
+# (https://www.odoo.com/documentation/user/12.0/legal/licenses/licenses.html#odoo-apps-license).
---
+++ test_module/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Versada UAB
+# Odoo Proprietary License v1.0 or later
+# (https://www.odoo.com/documentation/user/12.0/legal/licenses/licenses.html#odoo-apps-license).
+{
+    'name': 'Test Module',
+    'version': '12.0.1.0.0',
+    'author': 'Versada',
+    'category': 'Custom',
+    'website': 'https://versada.eu',
+    'license': 'OPL-1',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': False,
+}
